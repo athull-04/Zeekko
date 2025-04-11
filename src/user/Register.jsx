8687967d5e796ced27fb2d71db20ed5fdac5a972
ATHUL E R <athuler7@gmail.com>
2025-04-11 16:33:09 +0530

    Update Register.jsx

--- src/user/Register.jsx
+++ src/user/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("https://bzeekko.onrender.com/api/auth/reg/register", credentials, {
+      const response = await axios.post("https://bzeekko.onrender.com/api/auth/user/register", credentials, {
         headers: {
           "Content-Type": "application/json",
         },
