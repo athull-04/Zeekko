66e6b9dbc31cd6a5028eee50f116f2384c9957cd
ATHUL E R <athuler7@gmail.com>
2025-04-11 13:47:36 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://bzeekko.onrender.com/api";
 
 export const getProducts = async () => {
   try {
-    const response = await axios.get("http://localhost:5000/api/products");
+    const response = await axios.get("https://bzeekko.onrender.com/api/products");
     return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
